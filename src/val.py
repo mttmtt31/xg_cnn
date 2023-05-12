07a400bd5f0de2d2355ef1559a6d7852a562356d
mttmtt31 <matteomatteotti1@gmail.com>
2023-05-12 20:34:46 +0000

    refactoring: val is roc-auc

--- src/val.py
+++ src/val.py
@@ -1,28 +1,23 @@
 import torch
 from tqdm import tqdm
+from sklearn.metrics import roc_auc_score
 
-def val(model, val_loader, device, criterion, epoch):
+def val(model, val_loader, device, epoch):
     model.eval()
-    val_loss = 0.0
-    correct = 0
-    total = 0
+    xGs = []
+    outcomes = []
 
     with torch.no_grad():
         for images, labels in tqdm(val_loader, total = len(val_loader), desc = f'Validating epoch #{epoch+1}'):
+            # send to device
             images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
+            # append the xG
+            xGs.append(model(images).squeeze())
+            outcomes.append(labels)
 
-            # Calculate the loss
-            loss = criterion(outputs, labels)
-            val_loss = val_loss + loss.item()
-
-            # Track the total and correct predictions
-            _, predicted = torch.max(outputs.data, 1)
-            total = total + labels.size(0)
-            correct = correct + (predicted == labels).sum().item()
-
-        # Calculate average loss and accuracy on the validation set
-        val_loss = val_loss / len(val_loader)
-        val_acc = correct / total
-
-    return val_loss, val_acc
+        # calculate the roc-auc score
+        xGs = torch.cat(xGs).cpu()
+        outcomes = torch.cat(outcomes).cpu()
+        score = roc_auc_score(outcomes, xGs)
+        
+    return score
