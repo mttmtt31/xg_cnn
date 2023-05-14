fixed data leakage

main.py
@@ -1,10 +1,11 @@
 import torch.nn as nn
 import torch.optim as optim
-from src import XGCNN, FreezeFrameDataset, train, val
+from src import XGCNN, FreezeFrameDataset, train, val, train_val_split
 from torchvision import transforms
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader, Subset, random_split
 import wandb
 import argparse
+import random
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -45,24 +46,14 @@ def main(device, batch_size, lr, num_epochs, picture_type, log_wandb, augmentati
     folder_path = f'images/{picture_type}'
 
     # Load the dataset
-    dataset = FreezeFrameDataset(folder_path, transform=transform, augmentation=False)
+    dataset = FreezeFrameDataset(folder_path, transform=transform, augmentation=augmentation)
 
-    # Split the dataset into training and validation sets
-    train_size = int(0.8 * len(dataset))
-    val_size = len(dataset) - train_size
-    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
-
-    # Apply data augmentation (if needed) on the train_dataset.
-    # When augmentation, you need to play with train_dataset.dataset, because augmentation is at that level.
-    if augmentation:
-        train_dataset.dataset.samples = [sample for i, sample in enumerate(train_dataset.dataset.samples) if i in train_dataset.indices]
-        train_dataset.dataset.set_augmentation(True)
-        train_dataset = train_dataset.dataset
+    # Split train/val dataset
+    train_dataset, val_dataset = train_val_split(dataset=dataset, train_size=0.8)
 
     # Create data loaders
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-
 
     # Initialize the model
     model = XGCNN(dropout=dropout)

src/dataset.py
@@ -5,23 +5,23 @@ class FreezeFrameDataset(ImageFolder):
     def __init__(self, root, transform=None, target_transform=None, augmentation:bool=False):
         super().__init__(root, transform, target_transform)
         self.augmentation = augmentation
+        self.real_length = len(self.samples)
 
     def __getitem__(self, index):
         # check if augmentation is on
-        if self.augmentation:
-            index = index // 2
-        # retrieve image and label
-        path, target = self.samples[index]
+        if index >= self.real_length:
+            path, target = self.samples[index - self.real_length]
+        else:
+            # retrieve image and label
+            path, target = self.samples[index]
         image = self.loader(path)
 
         if self.transform is not None:
             image = self.transform(image)
 
         # perform data augmentation
-        if self.augmentation:
-            # check if index is even or odd. If odd -> flip along y-axis
-            if index % 2 == 1:
-                image = torch.flip(image, [-1]) 
+        if index >= self.real_length:
+            image = torch.flip(image, [-1]) 
 
         if self.target_transform is not None:
             target = self.target_transform(target)
@@ -31,6 +31,3 @@ class FreezeFrameDataset(ImageFolder):
 
     def __len__(self):
         return len(self.samples)*(self.augmentation+1)
-            
-    def set_augmentation(self, augmentation):
-        self.augmentation = augmentation