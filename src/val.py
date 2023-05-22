da6897d307cfb251592863b6fda77231ab5cbd9c
mttmtt31 <matteomatteotti1@gmail.com>
2023-05-22 08:40:42 +0000

    fix: val is with loss and roc/auc

--- src/val.py
+++ src/val.py
@@ -2,7 +2,7 @@ import torch
 from tqdm import tqdm
 from sklearn.metrics import roc_auc_score
 
-def val(model, val_loader, device, epoch):
+def val(model, val_loader, device, epoch, criterion):
     model.eval()
     xGs = []
     outcomes = []
@@ -11,13 +11,16 @@ def val(model, val_loader, device, epoch):
         for images, labels in tqdm(val_loader, total = len(val_loader), desc = f'Validating epoch #{epoch+1}'):
             # send to device
             images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+
             # append the xG
-            xGs.append(model(images).squeeze())
-            outcomes.append(labels)
+            xGs.append(outputs.squeeze())
+            outcomes.append(labels.float())
 
         # calculate the roc-auc score
         xGs = torch.cat(xGs).cpu()
         outcomes = torch.cat(outcomes).cpu()
-        score = roc_auc_score(outcomes, xGs)
+        roc_auc = roc_auc_score(outcomes, xGs)
+        log_loss = criterion(outcomes, xGs)
         
-    return score
+    return roc_auc, log_loss
