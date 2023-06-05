3c072ddcf473eb57fdc8a8afdbdbebbbb5a7dfab
mttmtt31 <matteomatteotti1@gmail.com>
2023-06-05 09:27:47 +0200

    add: matching between shot name and sb index

---
+++ src/utils/matching.py
@@ -0,0 +1,56 @@
+from statsbombpy import sb
+from tqdm import tqdm
+import warnings
+import json
+warnings.filterwarnings("ignore")
+import pandas as pd
+
+# create a json
+shots = {}
+# initialise variables
+num_shots = 0
+num_goals = 0
+
+# read competitions
+df_competitions = sb.competitions()
+# get rid of barcelona and invincible season
+# TODO: maybe we can consider their opponents' shot, at least?
+df_competitions = df_competitions[~df_competitions['competition_name'].isin(['La Liga', 'Premier League'])]
+
+# find games
+all_matches = []
+for i, row in tqdm(df_competitions.iterrows(), total = len(df_competitions), desc = 'Scanning competitions to find games'):
+    try:
+        all_matches.extend(sb.matches(competition_id=row['competition_id'], season_id=row['season_id'])['match_id'].tolist())
+    except:
+        continue
+
+print('Games have been individuated.')
+
+df_matching = []
+
+# keep track of how many shots
+num_shots = 0
+num_goals = 0
+# for every game
+for match_id in tqdm(all_matches, total = len(all_matches), desc = 'Scanning games, looking for shots.'):
+    # extract all shots
+    df_shots = sb.events(match_id, split=True)['shots']
+    # isolate open-play shots
+    df_shots = df_shots.loc[(~df_shots['shot_freeze_frame'].isna()) & (df_shots['shot_type'] == 'Open Play'), ['index', 'location', 'shot_freeze_frame', 'shot_outcome']].reset_index(drop = True)
+    for _, shot in df_shots.iterrows():
+        # create shot dictionary
+        outcome = shot["shot_outcome"].lower()
+        if outcome == 'goal':
+            shot_name = f'goals/{num_goals}'
+            num_goals = num_goals + 1
+        else:
+            shot_name = f'non_goals/{num_shots}'
+            num_shots = num_shots + 1
+        df_matching.append([shot_name, match_id, shot['index']])
+
+
+with open('data/shots.json', 'w') as f:
+    json.dump(shots, f)
+
+pd.DataFrame(df_matching, columns = ['shot_name', 'match_id', 'index']).to_csv('data/matching.csv', index = False)
