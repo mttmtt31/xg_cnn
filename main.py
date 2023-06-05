6d94827881d4ee98205e0c823bbad7deb117a75d
mttmtt31 <matteomatteotti1@gmail.com>
2023-06-05 14:39:15 +0000

    fix: train is repeated n times

--- main.py
+++ main.py
@@ -16,31 +16,12 @@ def parse_args():
     parser.add_argument('--wandb', action='store_true', help = 'Whether you want to log results in wandb')
     parser.add_argument('--optim', type = str, default = 'adam', help = 'Optimiser to use')
     parser.add_argument('--weight-decay', type = float, default = 0.0, help = 'Weight decay')
-    parser.add_argument('--angle', action = 'store_true', default = 'Whether to consider a channel for the shot angle')
+    parser.add_argument('--angle', action = 'store_true', help = 'Whether to consider a channel for the shot angle')
+    parser.add_argument('--n-runs', type = int, default = 1, help = 'How many runs')
 
     return parser.parse_args()
 
-def main(device, batch_size, lr, num_epochs, log_wandb, augmentation, angle, dropout, version, optimiser, wd):
-    if log_wandb:
-        # start a new wandb run to track this script
-        wandb.init(
-            # set the wandb project where this run will be logged
-            project="xg-cnn",
-            
-            # track hyperparameters and run metadata
-            config={
-            "learning_rate": lr,
-            "epochs": num_epochs,
-            "batch_size" : batch_size, 
-            "learning_rate" : lr, 
-            "angle" : angle,
-            "dropout" : dropout,
-            "version" : version,
-            "augmentation" : augmentation,
-            "optimiser" : optimiser,
-            "weight_decay" : wd
-            }
-        )
+def main(device, batch_size, lr, num_epochs, log_wandb, augmentation, angle, dropout, version, optimiser, wd, n_runs):
 
     data_path = 'data/shots.npy'
     labels_path = 'data/labels.npy'
@@ -62,16 +43,37 @@ def main(device, batch_size, lr, num_epochs, log_wandb, augmentation, angle, dro
     criterion = nn.BCELoss()
     optimizer = set_optimiser(model=model, optim=optimiser, learning_rate=lr, weight_decay=wd)
 
-    # Train the model
-    for i in range(num_epochs):
-        model, train_loss = train(train_loader=train_loader, model=model, epoch=i, device=device, optimizer=optimizer, criterion=criterion)
-
-        # evaluate the model on the validation set
-        roc_score, log_loss = val(model=model, val_loader=val_loader, device=device, epoch=i, criterion=criterion)
-
+    for _ in n_runs:
         # log in wandb
         if log_wandb:
-            wandb.log({"Train Loss": train_loss, "Validation ROC-AUC score:" : roc_score, "Validation loss" : log_loss})
+            # start a new wandb run to track this script
+            wandb.init(
+                # set the wandb project where this run will be logged
+                project="xg-cnn",
+                
+                # track hyperparameters and run metadata
+                config={
+                "learning_rate": lr,
+                "epochs": num_epochs,
+                "batch_size" : batch_size, 
+                "learning_rate" : lr, 
+                "angle" : angle,
+                "dropout" : dropout,
+                "version" : version,
+                "augmentation" : augmentation,
+                "optimiser" : optimiser,
+                "weight_decay" : wd
+                }
+            )
+        # Train the model
+        for i in range(num_epochs):
+            model, train_loss = train(train_loader=train_loader, model=model, epoch=i, device=device, optimizer=optimizer, criterion=criterion)
+
+            # evaluate the model on the validation set
+            roc_score, log_loss = val(model=model, val_loader=val_loader, device=device, epoch=i, criterion=criterion)
+
+            if log_wandb:
+                wandb.log({"Train Loss": train_loss, "Validation ROC-AUC score:" : roc_score, "Validation loss" : log_loss})
 
 if __name__ == '__main__':
     args = parse_args()
@@ -86,5 +88,6 @@ if __name__ == '__main__':
         version=args.version,
         optimiser=args.optim,
         wd = args.weight_decay,
-        angle = args.angle
+        angle = args.angle,
+        n_runs = args.n_runs
     )
