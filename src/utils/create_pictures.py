570f26b4902d138853fd367612e9a8356436260c
mttmtt31 <matteomatteotti1@gmail.com>
2023-06-05 09:23:18 +0200

    fix: pictures are now created on the offensive half on black bg

--- src/utils/create_pictures.py
+++ src/utils/create_pictures.py
@@ -18,6 +18,22 @@ def parse_args():
     
     return parser.parse_args()
 
+def area(x1, y1, x2, y2, x3, y3):
+    return abs((x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2))/2)
+
+def is_point_inside_triangle(ball, px, py):
+    x1, y1 = ball
+    x2, y2 = 120, 36
+    x3, y3 = 120, 44
+    triangle_area = area(x1, y1, x2, y2, x3, y3)
+    area1 = area(px, py, x2, y2, x3, y3)
+    area2 = area(x1, y1, px, py, x3, y3)
+    area3 = area(x1, y1, x2, y2, px, py)
+
+    if triangle_area - 0.01 <= area1 + area2 + area3 <= triangle_area + 0.01:
+        return True
+    else:
+        return False
 
 def main(s, plot_voronoi, plot_cones, plot_angle):
     with open('data/shots.json', 'rb') as f:
@@ -25,12 +41,21 @@ def main(s, plot_voronoi, plot_cones, plot_angle):
     for shot_name, shot in tqdm(dict_shots.items(), total = len(dict_shots), desc = 'Saving shot frames.......'):
         # isolate ball location
         ball = np.array(shot['ball'])
-        # isolate teammates location
-        teammates = np.array([player['location'] for key, player in shot.items() if key not in ['ball', 'outcome'] and player['teammate']]).reshape(-1, 2)
-        # isolate opponents location
-        opponents = np.array([player['location'] for key, player in shot.items() if key not in ['ball', 'outcome'] and not player['teammate'] and player['position']['name'] != 'Goalkeeper']).reshape(-1, 2)
-        # isolate gk location
-        gk = np.array([player['location'] for key, player in shot.items() if key not in ['ball', 'outcome'] and not player['teammate'] and player['position']['name'] == 'Goalkeeper']).reshape(-1, 2)
+        if plot_angle:
+            players_to_plot = {k:v for k,v in shot.items() if k not in ['ball', 'outcome', 'match_id', 'index'] and is_point_inside_triangle(ball, v['location'][0], v['location'][1])}
+            # isolate teammates location
+            teammates = np.array([player['location'] for key, player in players_to_plot.items() if player['teammate']]).reshape(-1, 2)
+            # isolate opponents location
+            opponents = np.array([player['location'] for key, player in players_to_plot.items() if not player['teammate'] and player['position']['name'] != 'Goalkeeper']).reshape(-1, 2)
+            # isolate gk location
+            gk = np.array([player['location'] for key, player in players_to_plot.items() if not player['teammate'] and player['position']['name'] == 'Goalkeeper']).reshape(-1, 2)
+        else:
+            # isolate teammates location
+            teammates = np.array([player['location'] for key, player in shot.items() if key not in ['ball', 'outcome'] and player['teammate']]).reshape(-1, 2)
+            # isolate opponents location
+            opponents = np.array([player['location'] for key, player in shot.items() if key not in ['ball', 'outcome'] and not player['teammate'] and player['position']['name'] != 'Goalkeeper']).reshape(-1, 2)
+            # isolate gk location
+            gk = np.array([player['location'] for key, player in shot.items() if key not in ['ball', 'outcome'] and not player['teammate'] and player['position']['name'] == 'Goalkeeper']).reshape(-1, 2)
         # put everything in a single dataframe with a column team which is a boolean for teammate or not. only for voronoi
         if plot_voronoi:
             opponents_gk = np.concatenate([opponents, gk])
@@ -38,7 +63,7 @@ def main(s, plot_voronoi, plot_cones, plot_angle):
             df_players['team'] = list(itertools.chain(*[[True]*len(teammates), [False]*(len(opponents_gk))]))
 
         # Setup the pitch
-        pitch = VerticalPitch(line_alpha=0, goal_alpha=0, pad_top=0, pad_bottom=0, pad_left=0, pad_right=0)
+        pitch = VerticalPitch(line_alpha=0, goal_alpha=0, pad_top=0, pad_bottom=0, pad_left=0, pad_right=0, half=True, pitch_color='black')
 
         # We will use mplsoccer's grid function to plot a pitch with a title axis.
         fig, axs = pitch.draw()
@@ -92,31 +117,30 @@ def main(s, plot_voronoi, plot_cones, plot_angle):
         # Set the figure size to 60x40 pixels
         fig.set_size_inches(1.5, 1.0)  # Inches are used for the size, so divide by the DPI
 
-        # Save the plot as a PNG image
         plt.subplots_adjust(left=0, right=1, bottom=0, top=1)
         # check if you need to plot voronoi for this goals
         if plot_voronoi:
             # save picture with all voronoi
-            plt.savefig(f'images/all/{shot_name}.png', dpi=200, bbox_inches='tight', pad_inches=0)
+            plt.savefig(f'images/all/{shot_name}.png', dpi=200, bbox_inches='tight', pad_inches=0, facecolor = 'black')
             # make the same picture but with only visible
             visible = pitch.polygon([np.array([[0,80.],[df_players[0].min() - 10,  80.], [df_players[0].min() - 10,  0], [0,0]])], color='white', ax=axs)
-            plt.savefig(f'images/visible/{shot_name}.png', dpi=200, bbox_inches='tight', pad_inches=0)
+            plt.savefig(f'images/visible/{shot_name}.png', dpi=200, bbox_inches='tight', pad_inches=0, facecolor = 'black')
         elif plot_cones and plot_angle:
             # save picture with cones and angles
             plt.gca().set_aspect('equal')
             # Save the plot as a PNG image
-            plt.savefig(f"images/cones+angle/{shot_name}.png", dpi=200, bbox_inches='tight', pad_inches=0)            
+            plt.savefig(f"images/cones+angle/{shot_name}.png", dpi=200, bbox_inches='tight', pad_inches=0, facecolor = 'black')            
         elif plot_cones:
             # Set the aspect ratio to 'equal' for circular circles
             plt.gca().set_aspect('equal')
             # Save the plot as a PNG image
-            plt.savefig(f"images/cones/{shot_name}.png", dpi=200, bbox_inches='tight', pad_inches=0)
+            plt.savefig(f"images/cones/{shot_name}.png", dpi=200, bbox_inches='tight', pad_inches=0, facecolor = 'black')
         elif plot_angle:
             # save picture with angle
-            plt.savefig(f'images/angle/{shot_name}.png', dpi=200, bbox_inches='tight', pad_inches=0)
+            plt.savefig(f'images/angle/{shot_name}.png', dpi=200, bbox_inches='tight', pad_inches=0, facecolor = 'black')
         else:
             # save picture with white background
-            plt.savefig(f'images/white/{shot_name}.png', dpi=200, bbox_inches='tight', pad_inches=0)
+            plt.savefig(f'images/white/{shot_name}.png', dpi=200, bbox_inches='tight', pad_inches=0, facecolor = 'black')
         # close the picture
         plt.close() 
 
